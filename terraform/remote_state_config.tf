64f88f0c293eb3c83428dff79ec7fa51f3d3dcd0
Austin Page <jaustinpage@gmail.com>
2022-02-17 11:40:53 -0600

    Fix terraform formatting

--- terraform/remote_state_config.tf
+++ terraform/remote_state_config.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.1.0"
     }
   }
   required_version = ">= 0.14.9"
   backend "s3" {
-    bucket = "tf-remote-state20220216232002199100000002"
-    key = "core/terraform.tfstate"
-    region = "us-east-2"
-    encrypt = true
-    kms_key_id = "d61a57b3-2242-4e28-b3f2-d743c95db1ac"
+    bucket         = "tf-remote-state20220216232002199100000002"
+    key            = "core/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    kms_key_id     = "d61a57b3-2242-4e28-b3f2-d743c95db1ac"
     dynamodb_table = "tf-remote-state-lock"
   }
 }
 
 provider "aws" {
   profile = "austininterview"
-  region = "us-east-2"
+  region  = "us-east-2"
 }
